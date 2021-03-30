981b9c3974fb7982436e3cc790c3108bd1dd6bf4
Stephen Chris <stephenchris91@gmail.com>
2021-03-30 15:46:49 -0700

    completed javascript functions and project is now complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,70 @@
-async function getQuote(){
- const url = 'https://swapi.dev/api/people/';
- try {
-     const getQuotes = axios.get
- } catch (error) {
-     
- }
+// const axios = require("axios");
+
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const twitter = document.querySelector('.twitter-button');
+const newQuote = document.querySelector('#new-quote');
+const authorText = document.querySelector('#author');
+const loader = document.querySelector('#loader');
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
 }
 
+function complete(){
+    if (!loader.hidden){
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+async function getQuote(){
+
+    loading();
+
+    let apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
+
+   try {
+    //    const response = await axios.get(apiUrl);
+    //    const data = response.data;
+    const response = await fetch(apiUrl);
+    const data = await response.json()
+          console.log(data);
+          if (data.quoteAuthor === ''){
+              data.quoteAuthor = 'Unknown'
+          }else{
+
+              quoteText.innerText = data.quoteText;
+              authorText.innerText = data.quoteAuthor;
+          }
+
+          //Reduce length of quote
+
+          if (data.quoteText.length > 120){
+              quoteText.classList.add('long-quote')
+          }else{
+              quoteText.classList.remove('long-quote');
+          }
+//stop loading and show quote
+
+          complete()
+
+   } catch (error) {
+       console.log('no data ', error)
+   }
+}
+
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const tweetURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+
+    window.open(tweetURL, '_blank');
+}
+
+newQuote.addEventListener('click', getQuote);
+twitter.addEventListener('click', tweetQuote);
+
+
 //onLoad 
-getQuote()
+getQuote();
